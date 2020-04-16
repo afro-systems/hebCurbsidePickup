83b1c2bba5131b1f5f3035aa7808c72f664340fc
Alex Turner <quietchallenger@396d855d-ce12-4a5b-ba56-2cbd7e99db05.local>
2020-04-15 20:55:18 -0400

    first commit

---
+++ curbside.ps1
@@ -0,0 +1,40 @@
+#!/bin/pwsh
+function Get-HEBCurbSideTimes {
+    param (
+        [string]$ZipCode,
+        [int]$Radius
+    )
+
+    $Url = "https://www.heb.com/commerce-api/v1/store/locator/address"
+    $ContentType = 'application/json'
+    $Body = @{
+        address                   =   $ZipCode
+        curbsideOnly              =   $true
+        radius                    =   $radius
+        nextAvailableTimeslot     =   $true
+        includeMedical            =   $false
+    } | ConvertTo-Json
+
+    $times = Invoke-RestMethod -Method Post -Uri $Url -Body $body -ContentType $ContentType
+    $list = @()    
+
+    foreach ($time in $times.stores){
+        if ($time.storeNextAvailableTimeslot.nextAvailableTimeslotDate) {
+            $t = [PSCustomObject]@{
+                Store                    = $time.store.name
+                NextAvailableTimeSlot    = $time.storeNextAvailableTimeslot.nextAvailableTimeslotDate
+                'Distance (mi)'          = [System.Math]::Round($time.distance, 2)
+                Address                  = $time.store.address1
+                'Store Hours'            = $time.store.storeHours
+            }
+            $list += $t 
+        }
+        else {
+            continue
+        }
+    }
+
+    return $list | Sort-Object -Property NextAvailableTimeSlot | Format-List
+}
+
+Get-HEBCurbSideTimes -ZipCode 78240 -Radius 5
